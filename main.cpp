72299ad28eca29af8ada7933cac5787e5358f0da
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2022-09-12 01:10:02 +0300

    debug version

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main() {
     struct stat a = {};
     stat("onegin.txt", &a);
-    int amount_of_symbols = a.st_size;
+    size_t amount_of_symbols = a.st_size;
 
     char onegin[amount_of_symbols] = {};
 
@@ -15,26 +15,37 @@ int main() {
     int c = '0';
     int amount_of_strings = 0;
 
-    for (int i = 0; i < amount_of_symbols && (c = getc(input)) != EOF; ++i) {
+    for (size_t i = 0; i < amount_of_symbols && (c = getc(input)) != EOF; ++i) {
         onegin[i] = (char) c;
-        if (c == '\n') {
+        if (onegin[i] == '\n') {
             ++c;
         }
     }
 
+    printf("%d\n", amount_of_strings);
+
     fclose(input);
+
+    for (int i = 0; i <101; ++i) {
+        printf("%c", onegin[i]);
+    }
 
     char *pointers_to_strings[amount_of_strings] = {};
     int n_string = 0;
 
-    for (int n_sym = 0; n_sym < amount_of_symbols; ++n_sym) {
+    //printf("%hhn", pointers_to_strings[1]);
+    //printf("%hhn", pointers_to_strings[2]);
+
+    for (size_t n_sym = 0; n_sym < amount_of_symbols; ++n_sym) {
+        printf(".");
         if (onegin[n_sym] == '\n') {
             pointers_to_strings[n_string] = &(onegin[n_sym]);
             ++n_string;
+            printf("\n");
         }
     }
 
-    printf("%c", pointers_to_strings[1][1]);
+    //printf("<%c>", pointers_to_strings[1][0]);
 
     bubble_sort(pointers_to_strings, amount_of_strings);
 
